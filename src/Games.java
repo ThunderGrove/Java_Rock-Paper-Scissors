67606f6f6ce21616d268898e5136612d2b06fd20
ThunderGrove <rene699f@edu.easj.dk>
2019-10-04 13:14:27 +0200

    Removed unneeded linebreaks and spaces.

--- src/Games.java
+++ src/Games.java
@@ -1,7 +1,6 @@
 public class Games{
     private int[]results={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    private int count=0;
-    private int mode=0;
+    private int count=0,mode=0;
     public void addResult(int result){
         //Add a result to array and count numbers added to array
         if((mode==0&&count<25)||(mode==1&&count<100)){//protection against adding more to the array than allowed
@@ -13,81 +12,63 @@ public class Games{
     public int numbersOfDraws(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==1||results[i]==4||results[i]==9){
-                count++;
-            }
+            if(results[i]==1||results[i]==4||results[i]==9){count++;}
         }
         return count;
     }
     public int numbersOfWins(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==3||results[i]==4||results[i]==8){
-                count++;
-            }
+            if(results[i]==3||results[i]==4||results[i]==8){count++;}
         }
         return count;
     }
     public int numbersOfLosses(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==2||results[i]==6||results[i]==7){
-                count++;
-            }
+            if(results[i]==2||results[i]==6||results[i]==7){count++;}
         }
         return count;
     }
     public int numbersOfRockPlayer(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==1||results[i]==2||results[i]==3){
-                count++;
-            }
+            if(results[i]==1||results[i]==2||results[i]==3){count++;}
         }
         return count;
     }
     public int numbersOfPaperPlayer(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==4||results[i]==5||results[i]==6){
-                count++;
-            }
+            if(results[i]==4||results[i]==5||results[i]==6){count++;}
         }
         return count;
     }
     public int numbersOfScissorsPlayer(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==7||results[i]==8||results[i]==9){
-                count++;
-            }
+            if(results[i]==7||results[i]==8||results[i]==9){count++;}
         }
         return count;
     }
     public int numbersOfRockCPU(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==1||results[i]==4||results[i]==7){
-                count++;
-            }
+            if(results[i]==1||results[i]==4||results[i]==7){count++;}
         }
         return count;
     }
     public int numbersOfPaperCPU(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==2||results[i]==5||results[i]==8){
-                count++;
-            }
+            if(results[i]==2||results[i]==5||results[i]==8){count++;}
         }
         return count;
     }
     public int numbersOfScissorsCPU(){
         int count=0;
         for(int i=0;i<results.length;i++){
-            if(results[i]==3||results[i]==6||results[i]==9){
-                count++;
-            }
+            if(results[i]==3||results[i]==6||results[i]==9){count++;}
         }
         return count;
     }
--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,24 @@ public class Main{
         System.out.print("Choose mode (0=One game vs CPU, 1=25 games vs CPU, 2=100 CPU vs CPU): ");
         int mode=input.nextInt();
         if(mode==0){
-            System.out.print("Choose between 0 (Rock), 1 (Paper) and 2 (Scissor): ");
-            int player=input.nextInt();
+            System.out.print("Choose between 0 (Rock), 1 (Paper) and 2 (Scissor): ");int player=input.nextInt();
             System.out.println(Rps.convertResult(Game.game(player)));
         }else if(mode==1){
-            Games games=new Games();
-            int i=0;
+            Games games=new Games();int i=0;
             while(i<25){
                 System.out.print("Choose between 0 (Rock), 1 (Paper) and 2 (Scissor): ");
-                int player=input.nextInt();
-                int result=Game.game(player);
+                int player=input.nextInt();int result=Game.game(player);
                 System.out.println(Rps.convertResult(result));
-                if(result!=-1){
-                    games.addResult(result);
-                    i++;
-                }
+                if(result!=-1){games.addResult(result);i++;}
             }
             System.out.println();
             System.out.println("Number of Draws: "+games.numbersOfDraws());
             System.out.println("Number of Wins: "+games.numbersOfWins());
             System.out.println("Number of Losses: "+games.numbersOfLosses());
         }else if(mode==2){
-            Games games=new Games();
-            games.setMode(1);
+            Games games=new Games();games.setMode(1);
             int cpuOne=0,i=0;
-            while(i<100){
-                cpuOne=(int)(Math.random()*3);
-                int result=Game.game(cpuOne);
-                if(result!=-1){
-                    games.addResult(result);
-                    i++;
-                }
-            }
+            while(i<100){cpuOne=(int)(Math.random()*3);int result=Game.game(cpuOne);if(result!=-1){games.addResult(result);i++;}}
             System.out.println();
             System.out.println("Number of Draws: "+games.numbersOfDraws());
             System.out.println("Number of CPU one Wins: "+games.numbersOfWins());
@@ -47,8 +33,6 @@ public class Main{
             System.out.println("CPU two selected Rock "+games.numbersOfRockCPU()+" times.");
             System.out.println("CPU two selected Paper "+games.numbersOfPaperCPU()+" times.");
             System.out.println("CPU two selected Scissors "+games.numbersOfScissorsCPU()+" times.");
-        }else{
-            System.out.println("No valid mode was selected");
-        }
+        }else{System.out.println("No valid mode was selected");}
     }
 }
